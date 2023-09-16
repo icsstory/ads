98743c7dc88dadeca4f60461947cbefe682d36fc
icsstory <12863279+icsstory@user.noreply.gitee.com>
2023-09-16 12:44:13 +0000

    31423432 234234
    Signed-off-by: icsstory <12863279+icsstory@user.noreply.gitee.com>

---
+++ ads.js
@@ -0,0 +1,12 @@
+{
+  "feedback": [
+    {
+      "img": "https://cdn.dribbble.com/users/2097465/screenshots/14831435/media/869803ba0b33dbef43baa0b22cfd383f.png?resize=1600x1200&vertical=center",
+      "click": "https://baidu.com"
+    },
+    {
+      "img": "https://cdn.dribbble.com/users/2097465/screenshots/15090818/media/aabe7850f9af090df1f0efe532b3a122.png?resize=1600x1200&vertical=center",
+      "click": "https://google.com"
+    }
+  ]
+}
